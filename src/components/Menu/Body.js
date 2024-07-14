462e99bcc88f8a01fc25eef9e136cdd93804f409
totsukatomofumi <totsukatomofumi@gmail.com>
2024-07-14 18:18:36 +0800

    Fix for ScrollTriggers to update on boxWidth change

--- src/components/Menu/Body.js
+++ src/components/Menu/Body.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Grid from "./Grid";
 import Box from "./Box";
 import Arrows from "./Arrows";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function Body() {
   const [boxWidth, setBoxWidth] = useState(0);
   const numBoxesCentered = 4; // TODO: change on resize
   const horizontalMargin = `calc((100vw - (${boxWidth}px + 0.75rem) * ${numBoxesCentered} - 0.75rem) / 2)`;
+
+  // fix for scrollTrigger to update on boxWidth change
+  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      ScrollTrigger.refresh();
+    },
+    { dependencies: [boxWidth] }
+  );
 
   return (
     <div className="relative w-full h-full">
@@ -22,7 +35,7 @@ function Body() {
         </div>
       </div>
 
-      <Arrows />
+      <Arrows boxWidth={boxWidth} />
     </div>
   );
 }
