9183beac011638574d658024edf157792315c242
totsukatomofumi <totsukatomofumi@gmail.com>
2024-12-10 05:48:22 +0800

    Refactor to use R3F primarily

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Environment, PerspectiveCamera, Helper } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
 import * as THREE from "three";
 import Map from "./models/Map";
 
 function App() {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen">
-      <Canvas frameloop="demand">
-        <Suspense fallback={null}>
-          <PerspectiveCamera
-            makeDefault
-            fov={40}
-            position={[27, 9, 0]}
-            rotation={
-              new THREE.Euler(
-                THREE.MathUtils.degToRad(-10),
-                THREE.MathUtils.degToRad(90),
-                0,
-                "YXZ"
-              )
-            }
-          />
-          <Map position={[0, 0, 0]} />
-          <Environment preset="sunset" />
-          <Helper type={THREE.GridHelper} />
-          <fog attach="fog" color="white" near={1} far={180} />
-        </Suspense>
+      <Canvas>
+        <Scene />
       </Canvas>
+
       <div className="absolute top-0 left-0 w-full h-full z-50 flex justify-center items-center">
         <div className="bg-black w-[1px] h-full"></div>
       </div>
@@ -35,6 +16,21 @@ function App() {
         <div className="bg-black w-full h-[1px]"></div>
       </div>
     </div>
+  );
+}
+
+function Scene() {
+  useThree(({ camera }) => {
+    camera.setFocalLength(50);
+    camera.position.set(0, 10, 40);
+    camera.rotation.set(THREE.MathUtils.degToRad(-10), 0, 0);
+  });
+  return (
+    <>
+      <Map position={[0, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
+      <ambientLight intensity={2} />
+      <fog attach="fog" args={["white", 0, 180]} />
+    </>
   );
 }
 
