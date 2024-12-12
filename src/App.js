5346c3f1a541242901a3a098ba203b45a705afa3
totsukatomofumi <totsukatomofumi@gmail.com>
2024-12-13 01:35:45 +0800

    Add touch joystick styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,120 @@
-import { act, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import * as THREE from "three";
 import Map from "./models/Map";
 import NavMesh from "./models/Navmesh";
 import playerSprite from "./sprites/player.png";
-import { useWindowSize } from "@uidotdev/usehooks";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function App() {
-  // pb-[80px]
-
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen ">
-      <div className="w-full h-full">
+    <div className="fixed top-0 left-0 w-screen h-screen pb-[80px] ">
+      <div className="relative w-full h-full">
         <Canvas>
           <Scene />
         </Canvas>
+        <Joystick />
+      </div>
+    </div>
+  );
+}
 
-        <div className="absolute top-0 left-0 w-full h-full z-50 flex justify-center items-center">
-          <div className="bg-black w-[1px] h-full"></div>
-        </div>
-        <div className="absolute top-0 left-0 w-full h-full z-50 flex justify-center items-center">
-          <div className="bg-black w-full h-[1px]"></div>
-        </div>
+function Joystick() {
+  const [isActive, setIsActive] = useState(false);
+  const [initTouchPos, setInitTouchPos] = useState([0, 0]);
+  const [currTouchPos, setCurrTouchPos] = useState([0, 0]);
+  const [touchId, setTouchId] = useState(null);
+  const baseRef = useRef();
+  const stickRef = useRef();
+
+  function handleOnTouchStart(e) {
+    const touch = e.changedTouches[0];
+
+    setTouchId(touch.identifier);
+    setInitTouchPos([touch.clientX, touch.clientY]);
+    setCurrTouchPos([touch.clientX, touch.clientY]);
+
+    setIsActive(true);
+  }
+
+  function handleOnTouchMove(e) {
+    let touch = null;
+
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      if (e.changedTouches[i].identifier === touchId) {
+        touch = e.changedTouches[i];
+        break;
+      }
+    }
+
+    if (touch === null) return;
+    setCurrTouchPos([touch.clientX, touch.clientY]);
+  }
+
+  function handleOnTouchEnd(e) {
+    setCurrTouchPos(initTouchPos);
+    setIsActive(false);
+  }
+
+  // joystick fade in
+  useGSAP(
+    () => {
+      if (baseRef.current === null) return;
+
+      const opacity = isActive ? 1 : 0;
+
+      const tl = gsap.timeline();
+
+      tl.to(baseRef.current, { opacity: opacity, duration: 0.1 });
+    },
+    { dependencies: [isActive] }
+  );
+
+  // joystick movement animation
+  useGSAP(
+    () => {
+      if (baseRef.current === null) return;
+
+      const tl = gsap.timeline();
+
+      tl.set(baseRef.current, { x: initTouchPos[0], y: initTouchPos[1] });
+      tl.set(stickRef.current, { x: 0, y: 0 });
+    },
+    { dependencies: [baseRef, initTouchPos] }
+  );
+
+  useGSAP(
+    () => {
+      if (stickRef.current === null) return;
+
+      const tl = gsap.timeline();
+
+      tl.to(stickRef.current, {
+        x: currTouchPos[0] - initTouchPos[0],
+        y: currTouchPos[1] - initTouchPos[1],
+        duration: 0.1,
+      });
+    },
+    { dependencies: [stickRef, currTouchPos, initTouchPos] }
+  );
+
+  return (
+    <div
+      className="absolute top-0 left-0 w-full h-full z-40"
+      onTouchStart={handleOnTouchStart}
+      onTouchEnd={handleOnTouchEnd}
+      onTouchMove={handleOnTouchMove}
+    >
+      <div
+        ref={baseRef}
+        className="absolute top-0 left-0 w-[100px] h-[100px] flex justify-center items-center translate-x-[-50px] translate-y-[-50px] opacity-0"
+      >
+        <div className="absolute top-0 left-0 w-full h-full opacity-50 bg-black rounded-full"></div>
+        <div
+          ref={stickRef}
+          className="w-[40px] h-[40px] opacity-50 bg-black rounded-full"
+        ></div>
       </div>
     </div>
   );
