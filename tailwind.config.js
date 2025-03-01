614fa1bacd42efce0039b8ef5f48ad4cd2ba0ac7
totsukatomofumi <totsukatomofumi@gmail.com>
2025-03-02 05:25:08 +0800

    Clear unused fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       vt323: ["VT323", "serif"],
-      cinzel: ["Cinzel", "serif"],
-      dotgothic16: ["DotGothic16", "sans-serif"],
-      shingoregular: ["Shin Go Regular", "sans-serif"],
-      baijamjuree: ["Bai Jamjuree", "sans-serif"],
-      roboto: ["Roboto", "sans-serif"],
-      dongle: ["Dongle", "sans-serif"],
-      macondo: ["Macondo", "sans-serif"],
-      oldenburg: ["Oldenburg", "serif"],
-      jacquard12: ["Jacquard 12", "system-ui"],
       synemono: ["Syne Mono", "monospace"],
-      syne: ["Syne", "sans-serif"],
     },
     extend: {
       colors: {
