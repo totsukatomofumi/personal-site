db826f7cd26c8c36ffb521845b857d5b44334705
totsukatomofumi <totsukatomofumi@gmail.com>
2025-03-02 05:31:17 +0800

    Adjust faux sprite shadow

--- src/components/SpriteShadow.jsx
+++ src/components/SpriteShadow.jsx
@@ -2,7 +2,7 @@ function SpriteShadow() {
   return (
     <mesh position={[0, -0.999, 0]} rotation={[-Math.PI / 2, 0, 0]}>
       <circleGeometry args={[0.6, 8]} />
-      <meshBasicMaterial color="black" transparent opacity={0.75} />
+      <meshBasicMaterial color="black" transparent opacity={0.7} />
     </mesh>
   );
 }
