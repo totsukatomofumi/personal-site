057b9e9df8452f45ce379a419ecc123f649a4d29
totsukatomofumi <totsukatomofumi@gmail.com>
2025-03-25 03:25:22 +0800

    Adjust contrast

--- src/components/Postprocessing.jsx
+++ src/components/Postprocessing.jsx
@@ -12,7 +12,7 @@ function Postprocessing() {
       <EffectComposer>
         <BrightnessContrast
           brightness={-0.05} // brightness. min: -1, max: 1
-          contrast={0.05} // contrast: min -1, max: 1
+          contrast={0.1} // contrast: min -1, max: 1
         />
         <Bloom luminanceThreshold={0.2} luminanceSmoothing={1} />
         <DepthOfField focusDistance={0.04} focalLength={0.1} bokehScale={3} />
