dc65245e75420274442a191b9fdc0ea9163604b5
totsukatomofumi <totsukatomofumi@gmail.com>
2025-03-26 15:58:05 +0800

    Remove noise due to issues

--- src/components/Postprocessing.jsx
+++ src/components/Postprocessing.jsx
@@ -21,7 +21,6 @@ function Postprocessing() {
         <HueSaturation
           saturation={-0.225} // saturation in radians
         />
-        <Noise opacity={0.02} />
       </EffectComposer>
       <SoftShadows />
     </>
