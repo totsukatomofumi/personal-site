9b2192f9b3fa4f5df22f0c5c952ca1aa2de96305
totsukatomofumi <totsukatomofumi@gmail.com>
2025-03-26 16:25:46 +0800

    Adjust postproc order

--- src/components/Postprocessing.jsx
+++ src/components/Postprocessing.jsx
@@ -16,12 +16,12 @@ function Postprocessing() {
           brightness={-0.05} // brightness. min: -1, max: 1
           contrast={0.1} // contrast: min -1, max: 1
         />
-        {/* <Bloom luminanceThreshold={0.2} luminanceSmoothing={1} /> */}
-        <DepthOfField focusDistance={0.04} focalLength={0.1} bokehScale={3} />
-        {/* <HueSaturation
+        <HueSaturation
           saturation={-0.225} // saturation in radians
-        /> */}
-        {/* <Noise opacity={0.02} /> */}
+        />
+        <Bloom luminanceThreshold={0.2} luminanceSmoothing={1} />
+        <DepthOfField focusDistance={0.04} focalLength={0.1} bokehScale={3} />
+        <Noise opacity={0.02} />
       </EffectComposer>
       <SoftShadows />
     </>
