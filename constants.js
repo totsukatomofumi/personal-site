24db459ae302cd2dab46563fec1e9f5587a4f620
totsukatomofumi <totsukatomofumi@gmail.com>
2025-12-31 14:53:37 +0800

    Update document parsing target JSON structure

--- constants.js
+++ constants.js
@@ -1,36 +1,36 @@
 import { createContext } from "react";
 
-function parseCardBlockContent(content) {
+function parseCardBlock(block) {
   const titleRegex = /title:\s*(.*?)(\s+\S*:|\s*$)/;
   const subtitleRegex = /subtitle:\s*(.*?)(\s+\S*:|\s*$)/;
   const coverRegex = /cover:\s*(.*?)(\s+\S*:|\s*$)/;
   const bodyRegex = /body:\s*(.*?)(\s+\S*:|\s*$)/;
   const extrasRegex = /extras:\s*(.*?)(\s+\S*:|\s*$)/;
 
-  const cardContent = {};
+  const cardVars = {};
 
-  const titleMatch = content.match(titleRegex);
+  const titleMatch = block.text.match(titleRegex);
   if (titleMatch) {
-    cardContent.title = titleMatch[1].trim();
+    cardVars.title = titleMatch[1].trim();
   }
 
-  const subtitleMatch = content.match(subtitleRegex);
+  const subtitleMatch = block.text.match(subtitleRegex);
   if (subtitleMatch) {
-    cardContent.subtitle = subtitleMatch[1].trim();
+    cardVars.subtitle = subtitleMatch[1].trim();
   }
 
-  const coverMatch = content.match(coverRegex);
+  const coverMatch = block.text.match(coverRegex);
   if (coverMatch) {
     const coverValue = coverMatch[1].trim();
 
     if (coverValue.includes(".")) {
-      cardContent.cover = {
+      cardVars.cover = {
         type: "image",
         url: coverValue,
       };
     } else {
       const dateParts = coverValue.split("-").map((part) => part.trim());
-      cardContent.cover = {
+      cardVars.cover = {
         type: "date",
         start: dateParts[0],
         end: dateParts[1],
@@ -38,69 +38,75 @@ function parseCardBlockContent(content) {
     }
   }
 
-  const bodyMatch = content.match(bodyRegex);
+  const bodyMatch = block.text.match(bodyRegex);
   if (bodyMatch) {
-    cardContent.body = bodyMatch[1].trim();
+    cardVars.body = bodyMatch[1].trim();
   }
 
-  const extrasMatch = content.match(extrasRegex);
+  const extrasMatch = block.text.match(extrasRegex);
   if (extrasMatch) {
     const extrasList = extrasMatch[1].split(",").map((extra) => extra.trim());
-    cardContent.extras = extrasList;
+    cardVars.extras = extrasList;
   }
 
-  return cardContent;
+  return {
+    type: "card",
+    ...cardVars,
+  };
 }
 
-function parseLinksBlockContent(content) {
+function parseLinksBlock(block) {
   const linkedinRegex = /linkedin:\s*(.*?)(\s+\S*:|\s*$)/;
   const githubRegex = /github:\s*(.*?)(\s+\S*:|\s*$)/;
   const kaggleRegex = /kaggle:\s*(.*?)(\s+\S*:|\s*$)/;
   const emailRegex = /email:\s*(.*?)(\s+\S*:|\s*$)/;
   const resumeRegex = /resume:\s*(.*?)(\s+\S*:|\s*$)/;
-  const linksContent = [];
+  const linksChildren = [];
 
-  const linkedinMatch = content.match(linkedinRegex);
+  const linkedinMatch = block.text.match(linkedinRegex);
   if (linkedinMatch) {
-    linksContent.push({
+    linksChildren.push({
       type: "linkedin",
       url: linkedinMatch[1].trim(),
     });
   }
 
-  const githubMatch = content.match(githubRegex);
+  const githubMatch = block.text.match(githubRegex);
   if (githubMatch) {
-    linksContent.push({
+    linksChildren.push({
       type: "github",
       url: githubMatch[1].trim(),
     });
   }
 
-  const kaggleMatch = content.match(kaggleRegex);
+  const kaggleMatch = block.text.match(kaggleRegex);
   if (kaggleMatch) {
-    linksContent.push({
+    linksChildren.push({
       type: "kaggle",
       url: kaggleMatch[1].trim(),
     });
   }
 
-  const emailMatch = content.match(emailRegex);
+  const emailMatch = block.text.match(emailRegex);
   if (emailMatch) {
-    linksContent.push({
+    linksChildren.push({
       type: "email",
       url: emailMatch[1].trim(),
     });
   }
 
-  const resumeMatch = content.match(resumeRegex);
+  const resumeMatch = block.text.match(resumeRegex);
   if (resumeMatch) {
-    linksContent.push({
+    linksChildren.push({
       type: "resume",
       url: resumeMatch[1].trim(),
     });
   }
 
-  return linksContent;
+  return {
+    type: "links",
+    children: linksChildren,
+  };
 }
 
 function parseDocument(document) {
@@ -114,7 +120,7 @@ function parseDocument(document) {
   let sections = document.split("---");
 
   sections = sections.map((section) => {
-    const sectionContent = [];
+    const sectionChildren = [];
 
     const lines = section
       .split("\n")
@@ -126,72 +132,72 @@ function parseDocument(document) {
     lines.forEach((line) => {
       if (headingRegex.test(line)) {
         if (currBlock) {
-          sectionContent.push(currBlock);
+          sectionChildren.push(currBlock);
           currBlock = null;
         }
 
-        sectionContent.push({
+        sectionChildren.push({
           type: "heading",
-          content: line.replace(headingRegex, "").trim(),
+          text: line.replace(headingRegex, "").trim(),
         });
       } else if (cardBlockStartRegex.test(line)) {
         currBlock = {
           type: "card",
-          content: "",
+          text: "",
         };
       } else if (linksBlockStartRegex.test(line)) {
         currBlock = {
           type: "links",
-          content: "",
+          text: "",
         };
       } else if (captionBlockStartRegex.test(line)) {
         currBlock = {
           type: "caption",
-          content: "",
+          text: "",
         };
       } else if (spacingBlockRegex.test(line)) {
         if (currBlock) {
-          sectionContent.push(currBlock);
+          sectionChildren.push(currBlock);
           currBlock = null;
         }
 
-        sectionContent.push({
+        sectionChildren.push({
           type: "spacing",
         });
       } else if (blockEndRegex.test(line)) {
         if (currBlock.type === "card") {
-          currBlock.content = parseCardBlockContent(currBlock.content);
+          currBlock = parseCardBlock(currBlock);
         } else if (currBlock.type === "links") {
-          currBlock.content = parseLinksBlockContent(currBlock.content);
+          currBlock = parseLinksBlock(currBlock);
         }
 
-        sectionContent.push(currBlock);
+        sectionChildren.push(currBlock);
         currBlock = null;
       } else {
         if (currBlock) {
-          currBlock.content += currBlock.content ? " " + line : line;
+          currBlock.text += currBlock.text ? " " + line : line;
         } else {
           currBlock = {
             type: "paragraph",
-            content: line,
+            text: line,
           };
         }
       }
     });
 
     if (currBlock) {
-      sectionContent.push(currBlock);
+      sectionChildren.push(currBlock);
     }
 
     return {
       type: "section",
-      content: sectionContent,
+      children: sectionChildren,
     };
   });
 
   return {
     type: "document",
-    content: sections,
+    children: sections,
   };
 }
 
@@ -337,6 +343,6 @@ libraries, and deployed with Vercel.
 
 export const DOCUMENT_JSON = parseDocument(DOCUMENT);
 
-export const NUM_SECTIONS = DOCUMENT_JSON.content.length;
+export const NUM_SECTIONS = DOCUMENT_JSON.children.length;
 
 export const APP_CONTEXT = createContext();
