2253edfa36e342233a650aafe0cdb7b76c84aa6b
totsukatomofumi <totsukatomofumi@gmail.com>
2026-01-10 22:41:07 +0800

    Add padding styling for Caption

--- src/components/Text/components/Caption/index.jsx
+++ src/components/Text/components/Caption/index.jsx
@@ -1,5 +1,9 @@
 function Caption({ text }) {
-  return <small className="no-split text-xs sm:text-sm">{text}</small>;
+  return (
+    <small className="no-split block text-xs sm:text-sm py-1.5 sm:py-2.5">
+      {text}
+    </small>
+  );
 }
 
 export default Caption;
