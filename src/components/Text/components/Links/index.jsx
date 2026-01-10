1448a42c46c7d46613a4d60f3e9381b819a85692
totsukatomofumi <totsukatomofumi@gmail.com>
2026-01-10 22:39:03 +0800

    Add hover styling for Links

--- src/components/Text/components/Links/index.jsx
+++ src/components/Text/components/Links/index.jsx
@@ -19,14 +19,24 @@ function Links({ children }) {
     email: faEnvelope,
   };
 
+  const titles = {
+    resume: "Resume",
+    linkedin: "LinkedIn",
+    github: "GitHub",
+    kaggle: "Kaggle",
+    email: "Email",
+  };
+
   return (
-    <nav className="no-split flex gap-3 text-lg">
+    <nav className="no-split flex gap-3">
       {children.map((child, index) => (
         <a
           key={index}
           href={child.url}
           target="_blank"
           rel="noopener noreferrer me"
+          className="text-lg py-2 border-y-2 border-transparent hover:border-b-inherit transition-[border]"
+          title={titles[child.type]}
         >
           <FontAwesomeIcon
             icon={icons[child.type] || faArrowUpRightFromSquare}
